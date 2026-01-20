5889a3a79092c4a1b192624600ccd0a3909da5cf
KUMAR, Aarav <akumarai@connect.ust.hk>
2026-01-20 14:04:47 +0800

    All Files Have been Requested. Updated Log.

--- OGE_downloadingForms/log.py
+++ OGE_downloadingForms/log.py
@@ -1,3 +1,7 @@
+"""
+This script logs the number of files requested and the page number for each person requested. Does not show the missing people.
+"""
+
 #load json to a python dict
 from collections import defaultdict
 import json
@@ -16,8 +20,6 @@ with open(PAGE_PATH, 'r') as f:
     all = json.load(f)
 
 
-#for those that are requested. How many files were requested? And What Page number are they from?
-
 #key: person. Value: [number of files requested, page number]
 log = {}
 
@@ -26,8 +28,19 @@ for k,v in all.items():
         log[k] = [len(reqs[k.lower()]), v]
 
 #save log to a csv
-with open('./audit/log.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Person', 'Number of Files Requested', 'Page Number'])
-    for k,v in log.items():
-        writer.writerow([k, v[0], v[1]])
+# with open('./audit/log.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['Person', 'Number of Files Requested', 'Page Number'])
+#     for k,v in log.items():
+#         writer.writerow([k, v[0], v[1]])
+
+#how many files have been requested? How Many people have been requested?
+people_count = 0
+files_count = 0
+
+for k,v in log.items():
+    people_count += 1
+    files_count += v[0]
+
+print(f"Total people requested: {people_count}")
+print(f"Total files requested: {files_count}")
